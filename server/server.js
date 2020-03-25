03ca6e5d3c7c46b01f2dbbc6c088657bbd052c8f
Dylan Smith <dylansmith86@gmail.com>
2020-03-25 12:02:22 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const cors = require("cors")
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+app.listen(8000, () => {
+    "Listening on port 8000"
+})
