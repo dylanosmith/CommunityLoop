472b5f521e714831a96e0cc5c6bac78d9a7b83cd
Callie Stansbury <calliestansbury@gmail.com>
2020-03-27 17:15:54 -0600

    newtaskform

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import LoginRegView from './views/LoginRegView';
 import NavAppBar from './components/Navigation/NavAppBar';
 import TaskDetail from './views/TaskDetail';
+import NewTask from './views/NewTask';
 import UserProfile from './views/UserProfile';
 import UserReviews from './views/UserReviews';
 import ManagePosts from './views/ManagePosts';
@@ -17,12 +18,12 @@ function App() {
       <NavAppBar />
       <Router>
         <LoginRegView path = "/" />
-        <Home path = "/home" />
+        {/* <Home path = "/home" /> */}
         <NewTask path = "/tasks/new" />
         <TaskDetail path = "/tasks/:id" />
-        <EditTask path = "/tasks/:id/edit" />
+        {/* <EditTask path = "/tasks/:id/edit" /> */}
         <UserProfile path = "/user/:id" />
-        <EditProfile path = "/user/:id/edit" />
+        {/* <EditProfile path = "/user/:id/edit" /> */}
         <UserReviews path = "/user/:id/reviews" />
         <ManagePosts path = "/user/:id/posts" />
         <ManageTasks path = "/user/:id/tasks" />
