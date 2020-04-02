185c2df14524c48b5501b156661c9441167886d4
Dylan Smith <dylansmith86@gmail.com>
2020-04-01 20:50:26 -0600

    fixed date formatting on task detail page

--- client/src/views/TaskDetail.js
+++ client/src/views/TaskDetail.js
@@ -3,15 +3,30 @@ import { Button } from '@material-ui/core';
 import axios from 'axios';
 
 const TaskDetail = props => {
-    const [taskState, setTaskState] = useState({});
+    const [taskState, setTaskState] = useState({
+        title: "",
+        description: "",
+        startDate: Date.now(),
+        completionDate: Date.now(),
+
+    });
+
+    const dateFormat = (date) => {
+        let taskDate = new Date(date);
+        return taskDate.getMonth() + "/" + taskDate.getDate() + "/" + taskDate.getFullYear()
+    }
 
     useEffect(() => {
         console.log("Firing axios. Props id --->", props.id);
         axios.get(`http://localhost:8000/api/tasks/${props.id}`)
         .then(res => {
             console.log("THIS IS THE RESPONSE:", res);
-            setTaskState(res.data.task);
-            console.log(res.data.task.startDate.toLocaleDateString());
+            setTaskState({
+                title: res.data.task.title,
+                description: res.data.task.description,
+                startDate: res.data.task.startDate,
+                completionDate:res.data.task.completionDate 
+            });
         })
         .catch(error =>
             console.log("SOMETHING IS WRONGGGGGG! HELP MEEEEE", error)
@@ -21,8 +36,8 @@ const TaskDetail = props => {
         <div>
             <h2>{ taskState.title }</h2>
             <div><p>Description: { taskState.description }</p></div>
-            <div><p>Start Date: { taskState.startDate }</p></div>
-            <div><p>Completion Date: {taskState.completionDate} </p></div>
+            <div><p>Start Date: { dateFormat(taskState.startDate) }</p></div>
+            <div><p>Completion Date: { dateFormat(taskState.completionDate) } </p></div>
         <Button variant="outline" color="primary">Back To Map</Button>
         <Button variant="outline" color="primary">Submit Bid</Button>
         <Button variant="outline" color="primary">Message Poster</Button>
